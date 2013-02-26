db53c69a117d75a5c665157d3c5709c7c891ae69
Tom <b1004537@my.shu.ac.uk>
2013-02-26 17:41:29 +0000

    grails-app/conf/BootStrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import uk.ac.shu.webarch.eRegister.*
+
+
 class BootStrap {
 
     def init = { servletContext ->
+    
+
+    	println("BootStrap::init");
+
+	def tom_instructor = Instructor.findByInstructorName('64535d'); 
+
+	println("Result of kind by staffid for tom: ${tom_instructor}");
+	
     }
+
     def destroy = {
     }
 }
