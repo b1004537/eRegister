db3884889346afb1b276bd68b850cafece94cbd4
Tom <b1004537@my.shu.ac.uk>
2013-03-19 18:00:06 +0000

    added code json and xml into classes controller and made index.gsp page in view

--- grails-app/controllers/uk/ac/shu/webarch/eRegister/ClassesController.groovy
+++ grails-app/controllers/uk/ac/shu/webarch/eRegister/ClassesController.groovy
@@ -1,6 +1,24 @@
 package uk.ac.shu.webarch.eRegister
 
+import grails.converters.*
+
 class ClassesController {
 
-    def index() { }
+    def index() { 
+	
+	def result=[
+	   classList:'one',
+	   b:'two'
+
+]
+
+	withFormat {
+	html result
+	xml { render result as XML }
+	json { render result as JSON }
+	
+}
+
+
+	}
 }
